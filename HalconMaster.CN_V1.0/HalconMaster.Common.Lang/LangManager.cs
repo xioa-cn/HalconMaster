e6b3a1f95c25979d17794dd146654c40db4e8f09
XIOA <1327916255@qq.com>
2025-04-08 22:06:40 +0800

    SetLanguage

--- HalconMaster.CN_V1.0/HalconMaster.Common.Lang/LangManager.cs
+++ HalconMaster.CN_V1.0/HalconMaster.Common.Lang/LangManager.cs
@@ -3,13 +3,41 @@ using HalconMaster.Common.Model.LangModels;
 
 namespace HalconMaster.Common.Lang;
 
-public class LangManager {
-    private static readonly ResourceDictionary ZhCn = new() {
+public class LangManager
+{
+    private static readonly ResourceDictionary ZhCn = new()
+    {
         Source = new Uri("pack://application:,,,/HalconMaster.Common.Lang;component/Langs/ZH-CN.xaml")
     };
-    private static readonly ResourceDictionary EnUs = new() {
+
+    private static readonly ResourceDictionary EnUs = new()
+    {
         Source = new Uri("pack://application:,,,/HalconMaster.Common.Lang;component/Langs/EN-US.xaml")
     };
-    public void SetLanguage(SystemLanguage lang) {
+
+    public static void SetLanguage(SystemLanguage lang)
+    {
+        var resources = Application.Current.Resources.MergedDictionaries;
+
+        var findResources = resources.Where(
+                x => x.Source?.ToString().Contains("/HalconMaster.Common.Lang;component/") ?? false)
+            .ToList();
+
+        foreach (var itemRes in findResources)
+        {
+            resources.Remove(itemRes);
+        }
+
+        switch (lang)
+        {
+            case SystemLanguage.EnUs:
+                resources.Add(EnUs);
+                break;
+            case SystemLanguage.ZhCn:
+                resources.Add(ZhCn);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(lang), lang, null);
+        }
     }
 }
