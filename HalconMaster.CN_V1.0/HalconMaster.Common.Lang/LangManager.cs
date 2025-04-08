d8304b955bb2b8675c76ce80891751eb9155fb5d
XIOA <1327916255@qq.com>
2025-04-08 22:01:50 +0800

    ResourceDictionary

--- HalconMaster.CN_V1.0/HalconMaster.Common.Lang/LangManager.cs
+++ HalconMaster.CN_V1.0/HalconMaster.Common.Lang/LangManager.cs
@@ -7,6 +7,9 @@ public class LangManager {
     private static readonly ResourceDictionary ZhCn = new() {
         Source = new Uri("pack://application:,,,/HalconMaster.Common.Lang;component/Langs/ZH-CN.xaml")
     };
+    private static readonly ResourceDictionary EnUs = new() {
+        Source = new Uri("pack://application:,,,/HalconMaster.Common.Lang;component/Langs/EN-US.xaml")
+    };
     public void SetLanguage(SystemLanguage lang) {
     }
 }
