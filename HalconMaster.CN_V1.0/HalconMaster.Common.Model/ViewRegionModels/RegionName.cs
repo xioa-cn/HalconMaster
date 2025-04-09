e94fea66f05e909151cdf49904e39809b11b0eab
XIOA <1327916255@qq.com>
2025-04-09 14:53:42 +0800

    Region property

--- HalconMaster.CN_V1.0/HalconMaster.Common.Model/ViewRegionModels/RegionName.cs
+++ HalconMaster.CN_V1.0/HalconMaster.Common.Model/ViewRegionModels/RegionName.cs
@@ -1,13 +1,12 @@
 ﻿namespace HalconMaster.Common.Model.ViewRegionModels;
 
 public static class RegionName {
-    public static string MainRegion { get; set; }
-    public static string HomeRegion { get; set; }
-    
+    public static string MainRegion { get; private set; }
+    public static string HomeRegion { get; private set; }
+
 
     static RegionName() {
         MainRegion = nameof(MainRegion);
         HomeRegion = nameof(HomeRegion);
-        
     }
 }
