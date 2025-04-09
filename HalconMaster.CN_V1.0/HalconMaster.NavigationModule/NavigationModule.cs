01b6554b2c2125274672c9e33fe71f3a0ba4e1e4
XIOA <1327916255@qq.com>
2025-04-09 14:58:02 +0800

    NavigationModule

---
+++ HalconMaster.CN_V1.0/HalconMaster.NavigationModule/NavigationModule.cs
@@ -0,0 +1,18 @@
+﻿using XPrism.Core.DI;
+using XPrism.Core.Modules;
+using XPrism.Core.Modules.Find;
+using XPrism.Core.Navigations;
+
+namespace HalconMaster.NavigationModule;
+
+[Module(nameof(NavigationModule))]
+public class NavigationModule : IModule {
+    public void RegisterTypes(IContainerRegistry containerRegistry) {
+        containerRegistry
+            .RegisterSingleton<INavigationService, NavigationService>(); 
+    }
+
+    public void OnInitialized(IContainerProvider containerProvider) {
+        throw new NotImplementedException();
+    }
+}
