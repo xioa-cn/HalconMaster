f0cce8aad9fad13e88402b25a2ae28ef6b3baedc
XIOA <1327916255@qq.com>
2025-04-11 11:31:35 +0800

    LoginWindowViewModel

--- HalconMaster.CN_V1.0/HalconMaster/ViewModels/LoginWindowViewModel.cs
+++ HalconMaster.CN_V1.0/HalconMaster/ViewModels/LoginWindowViewModel.cs
@@ -1,19 +1,36 @@
 ﻿using System.Windows;
 using CommunityToolkit.Mvvm.Input;
 using HalconMaster.Common.Model;
-using HalconMaster.Common.Tools.WindowsThemeTools;
+using HalconMaster.Common.Model.LoginModels;
+using HalconMaster.Common.ORM.EFDbContext.SystemDb;
 using HalconMaster.StartupDir;
-using XPrism.Core.DI;
+using Microsoft.EntityFrameworkCore;
 using XPrism.Core.Events;
 
 namespace HalconMaster.ViewModels;
 
-public partial class LoginWindowViewModel : BindableBase {
-    public LoginWindowViewModel(IEventAggregator eventAggregator) : base(eventAggregator) {
+public partial class LoginWindowViewModel
+    : BindableBase
+{
+    private readonly ISystemRepository _systemRepository;
+
+    public LoginWindowViewModel(IEventAggregator eventAggregator, ISystemRepository systemRepository) : base(
+        eventAggregator)
+    {
+        this._systemRepository = systemRepository;
+        this._eventAggregator.GetEvent<LoginEvent>()
+            .Subscribe<string, Task>(ReLogin, ThreadOption.UIThread, true, "ReLogin");
+    }
+
+    private async Task ReLogin(LoginModel arg)
+    {
     }
 
     [RelayCommand]
-    private void MainShow(Window window) {
+    private async Task MainShow(Window window)
+    {
+        var user = await _systemRepository.DbSet.FirstOrDefaultAsync(e => e.Name == "HalconMaster");
+        Console.WriteLine("Name:" + user.Name);
         Startup.MainStartup(window);
     }
 }
